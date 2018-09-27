4ae15cfd710e96eb0b33213d4cdde900d8712816
Sunstrider <mail4study@163.com>
2018-09-27 15:44:52 +0800

    sendmail927

--- main/sendMail.py
+++ main/sendMail.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import json, smtplib, os, requests, commands
+import json, smtplib, os, requests, commands, ConfigParser
 from email.mime.text import MIMEText
 
 def sendMail(content):
     mailhost = "dwmail.dangdang.com"
     mailuser = "ddop"
-    mailpass = "5GxIQSdGcV"
+    mailpass = "*"
     sender = 'ddop@dangdang.com'
 
     host = os.popen("/sbin/ifconfig|grep -A1 eth[01]|grep -m1 inet|awk -F: '{printf $2}'|awk '{printf $1}'").readline()
@@ -34,8 +34,15 @@ def sendMail(content):
         print(e)
         return False
 
+def getValues(section, option):
+    currDir = os.path.dirname(os.path.realpath(__file__))
+    confPath = currDir + os.sep + "logpath.conf"
+    config = ConfigParser.SafeConfigParser()
+    config.read(confPath)
+    return config.get(section=section, option=option)
+
 if __name__ == '__main__':
-    path = "/data/gceasy/jvm.log"
+    path = getValues('source', 'path')
     cmd = 'curl -s -X POST --data-binary @%s http://api.gceasy.io/analyzeGC?apiKey=6d79606b-28d1-4bf5-a03e-64e28b0422ea --header "Content-Encoding:zip"' % path
     status, result = commands.getstatusoutput(cmd)
     data = json.loads(result)
